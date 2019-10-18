5b42d17200675d1f0aa20d4877f1b122c7b7a4fc
LeanHydra84 <33231061+LeanHydra84@users.noreply.github.com>
2019-10-18 10:55:23 -0500

    Update charCont.cs

--- charCont.cs
+++ charCont.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class charCont : MonoBehaviour {
 	
+	CharacterController cc;
+	const float gravity = 20f;
+	const float jumpForce = 8f;
+	Vector3 moveDir = Vector3.zero;
+	
+	float mvY;
+	float mvX;
+	float mvZ;
+	
 	void Start() 
 	{
-		
+		cc = GetComponent<CharacterController>();
 	}
 	
 	void Update() 
 	{
+		if(Input.GetButtonDown("jump") && cc.isGrounded) 
+		{
+			mvY = jumpforce;
+		}
+		
+		mvY -= gravity * Time.deltaTime;
+		moveDir = new Vector3(mvX, mvY, mvZ);
+		cc.Move(moveDir * Time.deltaTIme);
 		
 	}
 	
