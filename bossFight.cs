d36968a8806a9810b7b24f9e1e600782c78d63c4
LeanHydra84 <LeanHydra84@users.noreply.github.com>
2019-11-21 17:55:31 -0600

    Changed shooting method

--- bossFight.cs
+++ bossFight.cs
@@ -30,6 +30,84 @@ public class bossFight : MonoBehaviour
         //
     }
 
+    void Attack(string att)
+    {
+        /*
+         Attacks List:
+         '-': Standard, shoots single bullet at you (sprayable)
+         '+': Shotgun, fires three offset bullets at you
+         '/': Ring of Fire, launches a ring around the entire map
+         '$': Shrapnel, single shot that collides with wall and does a miniature "Ring of Fire"
+         '!': Ricochet, shot bounces on collision ONCE
+         '@': IMPORTANTE, similar to single shot, but can be picked up and used for ammo        
+         */       
+
+        //Standard
+        if(att == "-")
+        {
+            Rigidbody proj = Instantiate(projectile, fire.position, transform.rotation);
+            proj.transform.rotation = smoothedRot;
+            proj.GetComponent<projectileScript>().damage = 1;
+            proj.AddForce(transform.forward * 30, ForceMode.Impulse);
+        }
+        //Shotgun
+        else if(att == "+")
+        {
+            /*
+            Quaternion adjustedRot = new Quaternion();
+            for (int i=0; i < 1; i++)
+            {
+                Rigidbody shotProj = Instantiate(projectile, fire.position, transform.rotation); //Wow this for loop won't work for what I want
+
+                if(i==1)
+                {
+                    adjustedRot = smoothedRot * Quaternion.Euler(Vector3.up * 15);
+                } 
+                else if(i==2)
+                {
+                    adjustedRot = smoothedRot * Quaternion.Euler(Vector3.up * -15);
+                }
+
+
+                shotProj.transform.rotation = adjustedRot;
+                shotProj.GetComponent<projectileScript>().damage = 1;
+                shotProj.AddForce(transform.forward * 30, ForceMode.Impulse);
+
+            }
+        */
+            Rigidbody shotProj = Instantiate(projectile, fire.position, transform.rotation);
+            Rigidbody shotProj1 = Instantiate(projectile, fire.position, transform.rotation * Quaternion.Euler(Vector3.up * 15));
+            Rigidbody shotProj2 = Instantiate(projectile, fire.position, transform.rotation * Quaternion.Euler(Vector3.up * -15));
+
+            shotProj.AddForce(shotProj.transform.forward * 30, ForceMode.Impulse);
+            shotProj1.AddForce(shotProj1.transform.forward * 30, ForceMode.Impulse);
+            shotProj2.AddForce(shotProj2.transform.forward * 30, ForceMode.Impulse);
+
+
+
+        }
+        //Ring of Fire
+        else if (att == "/")
+        {
+
+        }
+        //Shrapnel
+        else if (att == "$")
+        {
+
+        }
+        //Ricochet
+        else if (att == "!")
+        {
+
+        }
+        //Importante
+        else if (att == "@")
+        {
+
+        }
+    }
+
     IEnumerator IterateFile()
     {
         BPM = float.Parse(lines[0]);
@@ -39,6 +117,19 @@ public class bossFight : MonoBehaviour
         {
             for(int i=1; i < lines.Length; i++)
             {
+
+                if(float.TryParse(lines[i], out float waitBeats))
+                {
+                    waitBeats *= multiplier;
+                    yield return new WaitForSeconds(waitBeats);
+                } 
+                else
+                {
+                    Attack(lines[i]);
+                    yield return 0;
+                }
+
+                /*
                 Debug.Log(lines[i]);
                 if (lines[i] == "-")
                 {
@@ -46,14 +137,15 @@ public class bossFight : MonoBehaviour
                     proj.transform.rotation = smoothedRot;
                     proj.GetComponent<projectileScript>().damage = 1;
                     proj.AddForce(transform.forward * 30, ForceMode.Impulse);
-                    yield return 0;
+
                 }
                 else
                 {
                     float waitBeats = float.Parse(lines[i]);
                     waitBeats *= multiplier;
                     yield return new WaitForSeconds(waitBeats);
-                }
+                                       
+                } */
 
             }
 
